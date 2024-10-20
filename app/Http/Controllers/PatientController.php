f9c0b605a1b17074ad26a64742ede34ea05abfe8
Y24-Shaffan <yoosufshaffan24@gmail.com>
2024-10-20 08:00:33 +0500

    Created PatientController

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -2,9 +2,64 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Patient;
 use Illuminate\Http\Request;
 
 class PatientController extends Controller
 {
-    //
+    // GET
+    public function index()
+    {
+        return Patient::all();
+    }
+
+    // POST
+    public function store(Request $request)
+    {
+        $patient = new Patient();
+        $patient->fill($request->all());
+        $patient->save();
+        return response()->json([
+            "success" => true,
+            "data" => $patient
+        ]);
+    }
+
+    // GET a single patient by ID
+    public function show(string $id)
+    {
+        return Patient::find($id);
+    }
+
+    // PUT
+    public function update(Request $request, Patient $patient)
+    {
+        $request->validate([
+            'fname' => 'required|string|max:255',
+            'lname' => 'required|string|max:255',
+            'nid' => 'required|string|max:20',
+            'dob' => 'required|date',
+            'address_id' => 'required|exists:addresses,id',
+        ]);
+
+        $patient->update($request->all());
+
+        return response()->json([
+            'message' => 'Patient updated successfully',
+            'data' => $patient
+        ], 200);
+    }
+
+    // DELETE
+    public function destroy(string $id)
+    {
+        $patient = Patient::find($id);
+        if (!$patient) {
+            return response()->json(['message' => 'Patient not found'], 404);
+        }
+
+        $patient->delete();
+
+        return response()->json(['message' => 'Patient deleted successfully'], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IslandController;
 use App\Http\Controllers\AddressController;
+use App\Http\Controllers\PatientController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
@@ -19,3 +20,6 @@ Route::apiResource('islands', IslandController::class);
 
 // AddressController
 Route::apiResource('address', AddressController::class);
+
+// PatientController
+Route::apiResource('patient', PatientController::class);
